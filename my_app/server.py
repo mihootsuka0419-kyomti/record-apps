f01ab85a98f358112ed275a5036cc9025c9dd1f0
miho ootuka <mihootsuka0419@gmail.com>
2026-09-08 17:38:07 +0900

    サーバー起動

--- my_app/server.py
+++ my_app/server.py
@@ -4,14 +4,26 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 class MyHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        with open("templates/index.html", "r", encoding="utf-8") as f:
-            html = f.read()
 
-        self.send_response(200)
-        self.send_header("Content-type", "text/html; charset=utf-8")
-        self.end_headers()
+        if self.path == "/":
+            with open("templates/index.html", "r", encoding="utf-8") as f:
+                html = f.read()
 
-        self.wfile.write(html.encode("utf-8"))
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=utf-8")
+            self.end_headers()
+
+            self.wfile.write(html.encode("utf-8"))
+
+        elif self.path == "/static/style.css":
+            with open("static/style.css", "r", encoding="utf-8") as f:
+                css = f.read()
+
+            self.send_response(200)
+            self.send_header("Content-type", "text/css; charset=utf-8")
+            self.end_headers()
+
+            self.wfile.write(css.encode("utf-8"))
 
 
 server = HTTPServer(("", 8000), MyHandler)
