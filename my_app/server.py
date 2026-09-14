79473747a8441d13304a10b5f088eff674521463
miho ootuka <mihootsuka0419@gmail.com>
2026-09-14 16:39:39 +0900

    python

--- my_app/server.py
+++ my_app/server.py
@@ -1,21 +1,50 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
 
 
 class MyHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        with open("templates/index.html", "r", encoding="utf-8") as f:
-            html = f.read()
 
-        self.send_response(200)
-        self.send_header("Content-type", "text/html; charset=utf-8")
-        self.end_headers()
+        if self.path == "/":
+            file_path = os.path.join(
+                os.path.dirname(__file__),
+                "templates",
+                "index.html"
+            )
 
-        self.wfile.write(html.encode("utf-8"))
+            with open(file_path, "r", encoding="utf-8") as f:
+                html = f.read()
+
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=utf-8")
+            self.end_headers()
+
+            self.wfile.write(html.encode("utf-8"))
+
+        elif self.path == "/static/style.css":
+
+            file_path = os.path.join(
+                os.path.dirname(__file__),
+                "static",
+                "style.css"
+            )
+
+            with open(file_path, "r", encoding="utf-8") as f:
+                css = f.read()
+
+            self.send_response(200)
+            self.send_header("Content-type", "text/css; charset=utf-8")
+            self.end_headers()
+
+            self.wfile.write(css.encode("utf-8"))
 
 
-server = HTTPServer(("", 8000), MyHandler)
+server_address = ("", 8000)
 
-print("サーバー起動！")
+httpd = HTTPServer(server_address, MyHandler)
 
-server.serve_forever()
+print("🚀 サーバーを起動しました")
+print("http://localhost:8000")
+
+httpd.serve_forever()
